d8bd2a35be0f2cb704280c04b9b34feb180247f6
Yoshitomi Maehara <trombon2004@gmail.com>
2019-12-11 21:35:46 -0500

    clean repository

--- clean_folder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "\e[1;31mDeleting *.pyc and __pycache__\e[0m"
-echo "\e[1;95mCleaning in root folder\e[0m"
-rm *.pyc
-rm -r __pycache__
-echo "\e[1;95mCleaning in copper folder\e[0m"
-rm copper/*.pyc
-rm -r copper/__pycache__
