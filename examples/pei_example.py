b6c00f8dad13adb58e71787278a43372dce2fd6f
Yoshitomi Maehara <trombon2004@gmail.com>
2020-12-17 17:45:10 -0500

    change name of project and examples

---
+++ examples/pei_example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from ast import literal_eval
+import copper
+
+# path = '.'
+# path_res = '%s/pei_dataset_modif.csv' % path
+path_res = "pei_dataset_modif.csv"
+
+threshold = 3
+items_separated = False
+options = {'threshold': threshold, 'itemsSeparated': items_separated}
+
+data = pd.read_csv(path_res, sep=",", header=0, converters={"sequence": literal_eval})
+sids = list(data["sid"])
+sequences = list(data["sequence"])
+
+result_mining = copper.prefixspan(sequences, options)
+print(result_mining)
+# for l in result_mining:
+#    print(l)
