0a15aaa0e5b555c3bf17346c66afc98361d5821a
Yoshitomi Maehara <trombon2004@gmail.com>
2020-12-21 10:27:36 -0500

    update example by new package name

--- examples/pei_example.py
+++ examples/pei_example.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from ast import literal_eval
-import wincopper
+import wincopper as wc
 
 # path = '.'
 # path_res = '%s/pei_dataset_modif.csv' % path
@@ -15,7 +15,7 @@ data = pd.read_csv(path_res, sep=",", header=0, converters={"sequence": literal_
 sids = list(data["sid"])
 sequences = list(data["sequence"])
 
-result_mining = wincopper.prefixspan(sequences, options)
+result_mining = wc.prefixspan(sequences, options)
 print(result_mining)
 # for l in result_mining:
 #    print(l)
