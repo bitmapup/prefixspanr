68793832499ce4a4b5c90f1a07769069f87cd1fe
Yoshitomi Maehara <trombon2004@gmail.com>
2021-08-10 12:20:17 -0500

    fix metadata information

--- wincopper/metadata.py
+++ wincopper/metadata.py
@@ -3,14 +3,14 @@
 Package metadata
 """
 
-__name__ = "Copper"
+__name__ = "WinCopper"
 __author__ = ["Agustin Guevara Cogorno", "Hugo Alatrista Salas", "Miguel Nuñez del Prado"]
-__author_email__ = ["a.guevarac@up.edu.pe",]
+__author_email__ = ["a.guevarac@up.edu.pe"]
 __copyright__ = "Copyright 2020, BITMAP"
 __license__ = "GPL"
 __maintainer__ = "Yoshitomi Eduardo Maehara Aliaga"
 __maintainer_email__ = "ye.maeharaa@up.edu.pe"
 __credits__ = ["Agustin Guevara Cogorno", "Yoshitomi Eduardo Maehara Aliaga"]
 __institution_ = "Universidad del Pacifico"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __status__ = "Beta"
