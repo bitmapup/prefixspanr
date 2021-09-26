2478c659caac28eef29dd2d2ed33ae159822206c
Yoshitomi Maehara <trombon2004@gmail.com>
2021-09-26 18:59:13 -0500

    add setuptool into package requirements and fix some information about setup

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from setuptools import setup
 import setuptools
 import sys
 import wincopper.metadata as md
@@ -7,28 +6,29 @@ assert sys.version[0] == '2', "This package currently works in Python 2.x, soon 
 
 requires = []
 if sys.platform == 'win32':
-    requires = ['wheel', 'pandas', 'psutil', 'numpy']
+    requires = ['setuptools', 'wheel', 'psutil', 'pandas', 'numpy']
 elif sys.platform in ['linux', 'linux2', 'darwin', 'freebsd7', 
                       'freebsd8', 'freebsdN', 'openbsd6']:
-    requires = ['wheel', 'pandas', 'psutil', 'numpy', 'resource']
+    requires = ['setuptools', 'wheel', 'psutil', 'pandas', 'numpy', 'resource']
 else:
     assert requires != [], "OS not supported, Only supported in Windows and Unix-like"
     
-setup(
+setuptools.setup(
     name='wincopper',
     version=md.__version__,
     description='WinCopper Algorithm Implementation',
     url='https://github.com/bitmapup/prefixspanr',
     author=md.__author__,
     author_email=md.__author_email__,
+    contact=md.__contact__,
+    contact_email=md.__contact_email__,
     maintainer=md.__maintainer__,
     maintainer_email=md.__maintainer_email__,
     packages=setuptools.find_packages(),
     license=md.__license__,
     install_requires=requires,
-    zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: GNU Public License",
     ],
     python_requires='>=2.7'
